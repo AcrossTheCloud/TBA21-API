fd8c03d8b3dd878a1d4075b9f4f4145870fba64e
Matthew Berryman <matthew@acrossthecloud.net>
2018-06-29 09:59:12 +1000

    indentation

--- items.js
+++ items.js
@@ -6,8 +6,8 @@ const uuid = require('uuid/v1');
 const ocean = ['Pacific','Atlantic','Indian','Southern','Arctic'];
 
 const headers = {
-    "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-    "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+  "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+  "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
 };
 
 const addArtistNames = async (data) => {
@@ -33,119 +33,119 @@ const addArtistNames = async (data) => {
 };
 
 module.exports.get = async function(event, context, callback) {
-    console.log(event.queryStringParameters);
+  console.log(event.queryStringParameters);
 
-    try {
+  try {
 
     if (event.queryStringParameters === null) {
       let params = {
-          TableName : "tba21",
-          ProjectionExpression:"ocean, #tm, itemId, #p, description, #u, artistId",
-          ExpressionAttributeNames:{
-              "#p": "position",
-              "#u": "url",
-              "#tm": "timestamp"
-          }
+        TableName : "tba21",
+        ProjectionExpression:"ocean, #tm, itemId, #p, description, #u, artistId",
+        ExpressionAttributeNames:{
+          "#p": "position",
+          "#u": "url",
+          "#tm": "timestamp"
+        }
       };
 
       let data = await docClient.scan(params).promise();
       let withNames = await addArtistNames(data);
 
       const response = {
-          statusCode: 200,
-          headers: headers,
-          body: JSON.stringify(withNames),
+        statusCode: 200,
+        headers: headers,
+        body: JSON.stringify(withNames),
       };
       callback(null, response);
 
     } else if (typeof (event.queryStringParameters.ocean) === 'undefined') {
-        const response = {
-            statusCode: 400,
-            headers: headers,
-            body: 'invalid query parameter, try ?ocean=Pacific'
-        };
-        callback(null,response);
-    } else {
-        let params = {
-            TableName : "tba21",
-            ProjectionExpression:"ocean, #tm, itemId, #p, description, #u, artistId",
-            KeyConditionExpression: "ocean = :o",
-            ExpressionAttributeNames:{
-                "#p": "position",
-                "#u": "url",
-                "#tm": "timestamp"
-            },
-            ExpressionAttributeValues: {
-                ":o": event.queryStringParameters.ocean
-            }
-        };
-
-        let data = docClient.query(params).promise();
-        let withNames = await addArtistNames(data);
-        const response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(withNames),
-        };
-        callback(null, response);
-      }
-    } catch (error) {
       const response = {
-          statusCode: 503,
-          headers: headers,
-          body: JSON.stringify(error),
+        statusCode: 400,
+        headers: headers,
+        body: 'invalid query parameter, try ?ocean=Pacific'
+      };
+      callback(null,response);
+    } else {
+      let params = {
+        TableName : "tba21",
+        ProjectionExpression:"ocean, #tm, itemId, #p, description, #u, artistId",
+        KeyConditionExpression: "ocean = :o",
+        ExpressionAttributeNames:{
+          "#p": "position",
+          "#u": "url",
+          "#tm": "timestamp"
+        },
+        ExpressionAttributeValues: {
+          ":o": event.queryStringParameters.ocean
+        }
+      };
+
+      let data = docClient.query(params).promise();
+      let withNames = await addArtistNames(data);
+      const response = {
+        statusCode: 200,
+        headers: headers,
+        body: JSON.stringify(withNames),
       };
       callback(null, response);
     }
+  } catch (error) {
+    const response = {
+      statusCode: 503,
+      headers: headers,
+      body: JSON.stringify(error),
+    };
+    callback(null, response);
+  }
 
 };
 
 module.exports.post = function(event, context, callback) {
-    let body = JSON.parse(event.body);
+  let body = JSON.parse(event.body);
 
-    const schema = Joi.object().keys({
+  const schema = Joi.object().keys({
     ocean: Joi.any().valid(ocean).required(),
     description: Joi.string().required(),
     url: Joi.string().uri().required(),
     position: Joi.array().ordered([
-          Joi.number().min(-180).max(180).required(),
-          Joi.number().min(-90).max(90).required()
-      ]),
+      Joi.number().min(-180).max(180).required(),
+      Joi.number().min(-90).max(90).required()
+    ]),
     artistId: Joi.string().required()
-    });
+  });
 
-    if (!Joi.validate(body, schema).error) {
-        body.itemId = uuid();
-        body.timestamp = new Date() / 1000;
-        let putParams = {
-          TableName: "tba21",
-          Item: body
-        };
-        docClient.put(putParams, (error) => {
-            if (error) {
-                    const response = {
-                        statusCode: 503,
-                        headers: headers,
-                        body: JSON.stringify({ "message": "Server error " + error.toString() })
-                    };
-                    callback(null, response);
-                } else {
-                    console.log(event);
-                    const response = {
-                        statusCode: 200,
-                        headers: headers,
-                        body: JSON.stringify({ "message": "Item stored"})
-                    };
-                    callback(null, response);
-                }
-            });
-    } else {
+  if (!Joi.validate(body, schema).error) {
+    body.itemId = uuid();
+    body.timestamp = new Date() / 1000;
+    let putParams = {
+      TableName: "tba21",
+      Item: body
+    };
+    docClient.put(putParams, (error) => {
+      if (error) {
         const response = {
-            statusCode: 422,
-            headers: headers,
-            body: JSON.stringify({ "message": "Bad request, error validating body" })
+          statusCode: 503,
+          headers: headers,
+          body: JSON.stringify({ "message": "Server error " + error.toString() })
         };
         callback(null, response);
-    }
+      } else {
+        console.log(event);
+        const response = {
+          statusCode: 200,
+          headers: headers,
+          body: JSON.stringify({ "message": "Item stored"})
+        };
+        callback(null, response);
+      }
+    });
+  } else {
+    const response = {
+      statusCode: 422,
+      headers: headers,
+      body: JSON.stringify({ "message": "Bad request, error validating body" })
+    };
+    callback(null, response);
+  }
 
 };
