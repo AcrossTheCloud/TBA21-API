67f9c75b6fd6a70e5cbce918488ca360aab29c86
Matthew Berryman <matthew@acrossthecloud.net>
2018-06-28 14:30:14 +1000

    add artist names from artist table in items response

--- items.js
+++ items.js
@@ -10,6 +10,33 @@ const headers = {
     "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
 };
 
+const addArtistNames = function(data) {
+  data.Items = data.Items.map((item) => {
+    let params = {
+      TableName: "tba21-artists",
+      KeyConditionExpression: "artistId = :artistId",
+      ExpressionAttributeValues: {
+        ":artistId": item.artistId
+      }
+    };
+    docClient.query(params, function(err, data) {
+      if (err) {
+        const response = {
+          headers: headers,
+          statusCode: 503,
+          body: JSON.stringify(err)
+        };
+        callback(null,response);
+      } else {
+        console.log("Query succeeded.");
+        item['artistName']=response.Items[0].name;
+      }
+    });
+    return item;
+  });
+  return data;
+};
+
 module.exports.get = function(event, context, callback) {
     console.log(event.queryStringParameters);
 
@@ -36,7 +63,7 @@ module.exports.get = function(event, context, callback) {
               const response = {
                   statusCode: 200,
                   headers: headers,
-                  body: JSON.stringify(data),
+                  body: JSON.stringify(addArtistNames(data)),
               };
               callback(null, response);
           }
@@ -76,7 +103,7 @@ module.exports.get = function(event, context, callback) {
                 const response = {
                     statusCode: 200,
                     headers: headers,
-                    body: JSON.stringify(data),
+                    body: JSON.stringify(addArtistNames(data)),
                 };
                 callback(null, response);
             }
