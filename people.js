3de2e0d381a0f4afccc18349aae2546c03ce4c1c
Matthew Berryman <matthew@acrossthecloud.net>
2018-07-23 11:27:01 +1000

    copy and paste for now TODO: refactor

--- people.js
+++ people.js
@@ -9,6 +9,10 @@ const headers = {
   "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
 };
 
+function flattenDeep(arr1) {
+   return arr1.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
+}
+
 module.exports.get = async (event, context, callback) => {
   console.log(event.queryStringParameters);
 
