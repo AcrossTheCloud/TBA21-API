cf628c3bb420bff24731f820773621142d433451
Matthew Berryman <matthew@acrossthecloud.net>
2018-09-03 13:06:47 +1000

    method for getting image tags for uploaded image

---
+++ objects.js
@@ -0,0 +1,34 @@
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const headers = {
+  "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+  "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+};
+
+module.exports.tags = async (event, context, callback) => {
+  try {
+    let params = {
+      TableName : process.env.IMAGE_TAG_TABLE,
+      Key: {
+        HashKey: event.queryStringParameters.key
+      }
+    };
+    let data = await docClient.scan(params).promise();
+
+    const response = {
+      statusCode: 200,
+      headers: headers,
+      body: JSON.stringify(data),
+    };
+    callback(null, response);
+  } catch (error) {
+    console.log(error);
+    const response = {
+      statusCode: 503,
+      headers: headers,
+      body: JSON.stringify({ "message": "Server error " + error.toString() })
+    };
+    callback(null, response);
+  }
+};
