265b58c42cb8986b87e5244f427c7c4a1ec4020e
Matthew Berryman <matthew@acrossthecloud.net>
2018-10-08 10:02:17 +0800

    url should be an array to handle multiple uploads

--- items.js
+++ items.js
@@ -254,7 +254,7 @@ module.exports.post = async (event, context, callback) => {
     const schema = Joi.object().keys({
       ocean: Joi.any().valid(ocean).required(),
       description: Joi.string().required(),
-      url: Joi.string().uri().required(),
+      url: Joiarray().items(Joi.string().uri()).required(),
       position: Joi.array().ordered([
         Joi.number().min(-180).max(180).required(),
         Joi.number().min(-90).max(90).required()
