1449da4dff11855d42679c9c303b372036d8d82a
Zara <zara@acrossthecloud.net>
2019-05-28 14:09:17 +1000

    OC-61 get item by id sql statement and api function

--- src/items/items.test.ts
+++ src/items/items.test.ts
@@ -2,7 +2,7 @@ require('dotenv').config({ DEBUG: true });
 
 import { APIGatewayEvent, Context } from 'aws-lambda';
 import { db } from '../databaseConnect';
-import { getItems } from './items';
+import { getItems, getItemsById } from './items';
 
 afterAll( () => {
   // Close the database connection.
@@ -10,12 +10,19 @@ afterAll( () => {
 });
 
 describe('This is a simple test', () => {
-  test('Check the get function', async () =>{
+  test('Check the get function', async () => {
     const
       response = await getItems({} as APIGatewayEvent, {} as Context),
       item = JSON.parse(response.body);
 
-    expect(item.message[0].id).toEqual('3');
+    expect(item.message[0].id).toEqual('8');
 
+  });
+  test('testing the getItemsById function', async () => {
+    const
+        response = await getItemsById(8, {} as APIGatewayEvent, {} as Context,  ),
+        item = JSON.parse(response.body);
+
+    expect(item.message[0].id).toEqual('8');
   });
 });
--- src/items/items.ts
+++ src/items/items.ts
@@ -1,17 +1,73 @@
-import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import { APIGatewayEvent, APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { badRequestResponse } from '../common';
 import { db } from '../databaseConnect';
 
-export const getItems = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {// tslint:disable-line no-any
+export const getItems = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
+  let
+    queryString: { [name: string]: string } | null = event.queryStringParameters,
+    QUERY: string = `select COUNT ( id ) OVER ( ), * from ${process.env.DB_NAME}.items items`,
+    JOIN: string = ` INNER JOIN tba21.s3uploads s3uploads ON (items.s3uploads_sha512 = s3uploads.ID_sha512)`;
+
+  // Default query string params.
+  if (!queryString) {
+    queryString = {
+      limit: '15'
+    };
+  }
+
+  const
+    LIMIT: string = ` LIMIT ${queryString.limit}`,
+    OFFSET: string = queryString.hasOwnProperty('offset') ? ` OFFSET ${queryString.offset}` : '';
+
+  let WHERE: string = ` WHERE status=true`;
+  // if false (draft)
+    // todo get groups from AdminListGroupsForUser and check if user in apart of admin/specific group
+    // todo also check if the owner of this = the accessor
+
+  QUERY = QUERY + JOIN + WHERE + LIMIT + OFFSET;
+
   try {
     return {
       body: JSON.stringify({
-       message: await db.query(`select * from ${process.env.DB_NAME}.items`),
-     }),
+         message: await db.query(QUERY),
+       }),
       statusCode: 200,
     };
   } catch (e) {
     console.log('getItems ERROR - ', e);
     return badRequestResponse;
   }
+};
+
+export const getItemsById = async (id: number, event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+    if (id) {
+        let
+            queryString: { [name: string]: string } | null = event.queryStringParameters,
+            QUERY: string = `select COUNT ( id ) OVER ( ), * from ${process.env.DB_NAME}.items items`;
+
+        // Default query string params.
+        if (!queryString) {
+            queryString = {
+                limit: '15'
+            };
+        }
+        const
+            LIMIT: string = ` LIMIT ${queryString.limit}`,
+            OFFSET: string = queryString.hasOwnProperty('offset') ? ` OFFSET ${queryString.offset}` : '';
+
+        let WHERE: string = ` WHERE status=true AND id=${id}`;
+        QUERY = QUERY + WHERE + LIMIT + OFFSET;
+
+        try {
+            return {
+                body: JSON.stringify({
+                     message: await db.query(QUERY),
+                 }),
+                statusCode: 200,
+            };
+        } catch (e) {
+            console.log('getItems ERROR - ', e);
+            return badRequestResponse;
+        }
+    } else { return badRequestResponse ; }
 };
