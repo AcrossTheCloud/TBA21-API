a1fc53c82f1066f01ae69e3c05da867f91ed3bb6
Zara <zara@acrossthecloud.net>
2019-05-29 14:16:45 +1000

    OC-61 get item by tag sql statement and api function

--- src/items/items.ts
+++ src/items/items.ts
@@ -2,34 +2,25 @@ import { APIGatewayEvent, APIGatewayProxyEvent, APIGatewayProxyResult, Context }
 import { badRequestResponse } from '../common';
 import { db } from '../databaseConnect';
 
+/**
+ * Gets all the items
+ * @param event {APIGatewayEvent}
+ * @param context {Promise<APIGatewayProxyResult>}
+ */
 export const getItems = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
   let
     queryString: { [name: string]: string } | null = event.queryStringParameters,
     QUERY: string = `select COUNT ( id ) OVER ( ), * from ${process.env.DB_NAME}.items items`,
-    JOIN: string = ` INNER JOIN tba21.s3uploads s3uploads ON (items.s3uploads_sha512 = s3uploads.ID_sha512)`;
-
-  // Default query string params.
-  if (!queryString) {
-    queryString = {
-      limit: '15'
-    };
-  }
+    JOIN: string = `INNER JOIN tba21.s3uploads s3uploads ON (items.s3uploads_sha512 = s3uploads.ID_sha512)`;
 
   const
-    LIMIT: string = ` LIMIT ${queryString.limit}`,
-    OFFSET: string = queryString.hasOwnProperty('offset') ? ` OFFSET ${queryString.offset}` : '';
-
-  let WHERE: string = ` WHERE status=true`;
-  // if false (draft)
-    // todo get groups from AdminListGroupsForUser and check if user in apart of admin/specific group
-    // todo also check if the owner of this = the accessor
-
-  QUERY = QUERY + JOIN + WHERE + LIMIT + OFFSET;
+    LIMIT: string = (queryString && queryString.hasOwnProperty('limit') && parseInt(queryString.limit, 0) < 50) ? `LIMIT ${queryString.limit}` : 'LIMIT 15',
+    OFFSET: string = (queryString && queryString.hasOwnProperty('offset')) ? `OFFSET ${queryString.offset}` : '';
 
   try {
     return {
       body: JSON.stringify({
-         message: await db.query(QUERY),
+         message: await db.query(`${QUERY} ${JOIN} WHERE status=true ${LIMIT} ${OFFSET}`),
        }),
       statusCode: 200,
     };
@@ -38,36 +29,69 @@ export const getItems = async (event: APIGatewayProxyEvent, context: Context): P
     return badRequestResponse;
   }
 };
+/**
+ * Gets the item by their id
+ * @param event {APIGatewayEvent}
+ * @param context {Promise<APIGatewayProxyResult>}
+ */
+export const getItemsById = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+  let
+    queryString: { [name: string]: string } | null = event.queryStringParameters,
+    QUERY: string = `select COUNT ( id ) OVER ( ), * from ${process.env.DB_NAME}.items items`;
 
-export const getItemsById = async (id: number, event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    if (id) {
-        let
-            queryString: { [name: string]: string } | null = event.queryStringParameters,
-            QUERY: string = `select COUNT ( id ) OVER ( ), * from ${process.env.DB_NAME}.items items`;
+  // Default query string params.
+  if (queryString && queryString.hasOwnProperty('id')) {
+    const
+      LIMIT: string = (queryString.hasOwnProperty('limit') && parseInt(queryString.limit, 0) < 50) ? `LIMIT ${queryString.limit}` : 'LIMIT 15',
+      OFFSET: string = queryString.hasOwnProperty('offset') ? `OFFSET ${queryString.offset}` : '';
 
-        // Default query string params.
-        if (!queryString) {
-            queryString = {
-                limit: '15'
-            };
-        }
-        const
-            LIMIT: string = ` LIMIT ${queryString.limit}`,
-            OFFSET: string = queryString.hasOwnProperty('offset') ? ` OFFSET ${queryString.offset}` : '';
+    try {
+      return {
+        body: JSON.stringify({
+           message: await db.query(`${QUERY} WHERE status=true AND id=${queryString.id} ${LIMIT} ${OFFSET}`),
+        }),
+        statusCode: 200,
+      };
+    } catch (e) {
+      console.log('getItemsById ERROR - ', e);
+      return badRequestResponse;
+    }
+  } else {
+    return badRequestResponse ;
+  }
+};
+/**
+ * Gets the item by tag id
+ * @param event {APIGatewayEvent}
+ * @param context {Promise<APIGatewayProxyResult>}
+ */
+export const getItemsByTag = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+  let
+    queryString: { [name: string]: string } | null = event.queryStringParameters,
+    QUERY: string = `select COUNT ( ID ) OVER ( ), * from ${process.env.DB_NAME}.items items`;
 
-        let WHERE: string = ` WHERE status=true AND id=${id}`;
-        QUERY = QUERY + WHERE + LIMIT + OFFSET;
+  if (queryString) {
+    const
+      KEYWORD = queryString.hasOwnProperty('keywordTag') ? `AND (keyword_tags::text like ('{%${queryString.keywordTag}%}'))` : '',
+      CONCEPT = queryString.hasOwnProperty('conceptTag') ? `AND (concept_tags::text like ('{%${queryString.conceptTag}%}'))` : '';
 
-        try {
-            return {
-                body: JSON.stringify({
-                     message: await db.query(QUERY),
-                 }),
-                statusCode: 200,
-            };
-        } catch (e) {
-            console.log('getItems ERROR - ', e);
-            return badRequestResponse;
-        }
-    } else { return badRequestResponse ; }
+    const
+      LIMIT: string = (queryString.hasOwnProperty('limit') && parseInt(queryString.limit, 0) < 50) ? `LIMIT ${queryString.limit}` : 'LIMIT 15',
+      OFFSET: string = queryString.hasOwnProperty('offset') ? `OFFSET ${queryString.offset}` : '';
+
+    try {
+      return {
+        body: JSON.stringify({
+           message: await db.query(`${QUERY} WHERE status=true ${KEYWORD} ${CONCEPT} ${LIMIT} ${OFFSET}`),
+         }),
+        statusCode: 200,
+      };
+    } catch (e) {
+      console.log('getItems ERROR - ', e);
+      return badRequestResponse;
+    }
+
+  } else {
+    return badRequestResponse;
+  }
 };
