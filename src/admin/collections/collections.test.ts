e543360798c681a25027902ee15de70f052b24ba
Dan Wood <dan@flix.link>
2019-06-12 10:26:19 +1000

    Revert "Oc 59 queries and testing"

--- src/admin/collections/collections.test.ts
+++ src/admin/collections/collections.test.ts
@@ -8,7 +8,7 @@ require('dotenv').config(
 import { APIGatewayProxyEvent, Context } from 'aws-lambda';
 import { db } from '../../databaseConnect';
 import { QueryStringParameters } from '../../types/_test_';
-import { get, getById, getByTag, getByPerson } from './collections';
+import { get, getById, getByTag } from './collections';
 
 afterAll( () => {
   // Close the database connection.
@@ -93,23 +93,6 @@ describe('/collections/getByTag', () => {
     const
       queryStringParameters: QueryStringParameters = {tag: ''},
       response = await getByTag({queryStringParameters } as APIGatewayProxyEvent, {} as Context);
-
-    expect(response.statusCode).toEqual(400);
-  });
-});
-
-describe('/collections/getByPerson', () => {
-  test('Get all collections with a person of Tim', async () => {
-    const
-      queryStringParameters: QueryStringParameters = {person: 'Tim'},
-      response = await getByPerson({queryStringParameters } as APIGatewayProxyEvent, {} as Context),
-      result = JSON.parse(response.body);
-    expect(result.collections.length).toEqual(1);
-  });
-  test('Get a bad response when no person is given', async () => {
-    const
-      queryStringParameters: QueryStringParameters = {person: ''},
-      response = await getByPerson({queryStringParameters } as APIGatewayProxyEvent, {} as Context);
 
     expect(response.statusCode).toEqual(400);
   });
