b0ba81b54748c05617a3f2c0470b91529e2dfe83
Dan Wood <daniel@acrossthecloud.net>
2019-07-10 13:30:26 +1000

    OC-156 Insert multiple tags into DB, returning their details afterwards With tests.

--- src/tags/tags.ts
+++ src/tags/tags.ts
@@ -25,7 +25,7 @@ export const get = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyR
       sqlStatement = `
         SELECT * 
           FROM ${type === 'concept' ? process.env.CONCEPT_TAGS_TABLE : process.env.KEYWORD_TAGS_TABLE}
-        WHERE Lower(tag_name) LIKE '%' || LOWER($1) || '%'
+        WHERE LOWER(tag_name) LIKE '%' || LOWER($1) || '%'
       `;
 
     const result = await db.any(sqlStatement, params);
@@ -33,6 +33,55 @@ export const get = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyR
     return successResponse({ tags: result ? result : [] });
   } catch (e) {
     console.log('/tags/tags.get ERROR - ', e);
+    return badRequestResponse();
+  }
+};
+
+/**
+ *
+ * Insert Tags from an array of strings
+ *
+ * @param event {APIGatewayEvent}
+ *
+ * @returns { Promise<APIGatewayProxyResult> } an array of tag objects {id: number, tag_name: string}
+ */
+export const insert = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const data = JSON.parse(event.body);
+
+    await Joi.validate(data, Joi.object().keys({
+      type: Joi.string().valid('keyword', 'concept').required(),
+      tags: Joi.array().items(Joi.string()).required()
+    }));
+
+    const
+      { type, tags } = data,
+      tableName = type === 'concept' ? process.env.CONCEPT_TAGS_TABLE : process.env.KEYWORD_TAGS_TABLE,
+      sqlStatement = `
+        WITH tag AS
+        (
+          SELECT * FROM ${tableName} WHERE LOWER(tag_name) = LOWER($1)
+        ),
+        i AS
+        (
+          INSERT INTO ${tableName}(tag_name)
+            VALUES ( LOWER($1) ) ON CONFLICT (tag_name) DO NOTHING
+          RETURNING id, tag_name
+        )
+
+        SELECT * FROM tag UNION ALL SELECT * FROM i;
+      `;
+
+    const results = [];
+    // Loop through each tag and do a query, returning the tag object and pushing it into a final array
+    for (const tag of tags) {
+      const result = await db.one(sqlStatement, tag);
+      results.push(result);
+    }
+
+    return successResponse({ tags: results });
+  } catch (e) {
+    console.log('/tags/tags.insert ERROR - ', e);
     return badRequestResponse();
   }
 };
