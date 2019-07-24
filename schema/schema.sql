a687362b4a4eff24cd716e89c0b57d0bed4607c3
Dan Wood <daniel@acrossthecloud.net>
2019-07-24 12:52:01 +1000

    Fixed up enum

--- schema/schema.sql
+++ schema/schema.sql
@@ -22,7 +22,7 @@ CREATE TYPE tba21.item_type AS ENUM ('Video', 'Text', 'Audio', 'Image');
 --Item subtypes
 CREATE TYPE tba21.subtype AS ENUM (
   'Music', 'Performance', 'Sound Art', 'Lecture', 'Radio', 'Interview', 'Field Recording', 'Podcast',
-  'Academic Public', 'Article', 'News', 'Policy Paper', 'Report', 'Book', 'Essay', 'Historical Text', 'Event Press', 'Toolkit', 'Other',
+  'Academic Publication', 'Article', 'News', 'Policy Paper', 'Report', 'Book', 'Essay', 'Historical Text', 'Event Press', 'Toolkit', 'Other',
   'Movie', 'Documentary', 'Art', 'Journalism', 'Event Recording', 'Informational Video', 'Trailer',
   'Artwork Documentation', 'Raw Footage', 'Photograph', 'Research', 'Digital Art', 'Graphics', 'Map', 'Film Still', 'Sculpture',
   'Painting', 'Illustration'
