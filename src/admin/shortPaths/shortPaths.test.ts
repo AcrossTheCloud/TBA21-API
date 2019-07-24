aca3aa6e6fd779b61f8a5cfa1b0de27c84568ebd
Zara <zara@acrossthecloud.net>
2019-07-24 13:15:18 +1000

    OC-172 insert and updated the get

---
+++ src/admin/shortPaths/shortPaths.test.ts
@@ -0,0 +1,45 @@
+import { QueryStringParameters } from '../../types/_test_';
+
+require('dotenv').config(
+  {
+    DEBUG: true
+  }
+);
+
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { db } from '../../databaseConnect';
+import { reSeedDatabase } from '../../utils/testHelper';
+import { get, insert } from './shortPaths';
+
+describe('admin/shortPaths/shortpaths/get', () => {
+  // AfterAll tests reseed the DB
+  afterAll( async () => {
+    await reSeedDatabase();
+    // Close the database connection.
+    db.$pool.end();
+  });
+
+  test('get for short paths', async () => {
+    const
+      queryStringParameters: QueryStringParameters = {
+        'table': 'Item',
+        'short_path': 'Kitten'
+      },
+      response = await get({queryStringParameters} as APIGatewayProxyEvent),
+      responseBody = JSON.parse(response.body);
+    console.log(response, responseBody, );
+    expect(responseBody);
+  });
+  test('insert for short paths', async () => {
+    const
+      requestBody = {
+        'short_path': 'new',
+        'id': '6',
+        'object_type': 'Profile'
+      },
+      body: string = JSON.stringify(requestBody),
+      response = await insert({ body } as APIGatewayProxyEvent),
+      responseBody = JSON.parse(response.body);
+    expect(responseBody.short_path).toEqual({'short_path': 'new'});
+  });
+});
--- src/admin/shortPaths/shortPaths.ts
+++ src/admin/shortPaths/shortPaths.ts
@@ -1,28 +1,91 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { badRequestResponse, successResponse } from '../../common';
+import { badRequestResponse, headers, internalServerErrorResponse, successResponse } from '../../common';
 import { db } from '../../databaseConnect';
 import Joi from '@hapi/joi';
 
 export const get = async(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
     await Joi.validate(event.queryStringParameters, Joi.object().keys({
-      table: Joi.string().valid('profiles', 'collections', 'items').required(),
-      id: Joi.number().integer().required()
+      table: Joi.string().valid('Profile', 'Collection', 'Item'),
+      short_path: Joi.string().required()
     }));
+    const queryString = event.queryStringParameters;
+    let table = process.env.ITEMS_TABLE;
+
+    switch (queryString.table) {
+      case 'profiles':
+        table =  process.env.PROFILES_TABLE;
+        break;
+      case 'collections':
+        table =  process.env.COLLECTIONS_TABLE;
+        break;
+      default:
+        break;
+    }
 
     const
-      queryString = event.queryStringParameters,
-      params = [queryString.table, queryString.id],
+      params = [queryString.table, queryString.short_path],
       sqlStatement = `
         SELECT *
-        FROM ${process.env.SHORT_PATHS}
-        JOIN ${queryString.table === 'profiles' ? process.env.PROFILES_TABLE : (queryString.table === 'collections' ? process.env.COLLECTIONS_TABLE : process.env.ITEMS_TABLE)}
-        AS id_ on ${process.env.SHORT_PATHS}.id = id_.id
-        WHERE ${process.env.SHORT_PATHS}.id= $2 AND id_.id=$2
+        FROM (
+          SELECT * 
+          FROM ${process.env.SHORT_PATHS}
+          WHERE ${process.env.SHORT_PATHS}.short_path = $2
+        ) AS short_paths
+          JOIN ${table}
+          AS id_ on short_paths.id = id_.id
       `;
     return successResponse({short_path: await db.any(sqlStatement, params)});
   } catch (e) {
     console.log('/profiles/shortPaths.get ERROR - ', e);
     return badRequestResponse();
+  }
+};
+
+export const insert = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const data = JSON.parse(event.body);
+
+    await Joi.validate(data, Joi.object().keys(
+      {
+        short_path: Joi.string().required(),
+        id: Joi.number().required(),
+        object_type: Joi.string()
+      }));
+
+    let paramCounter = 0;
+
+    const
+      params = [],
+      sqlFields: string[] = Object.keys(data).map((key) => {
+        return `${key}`;
+      }),
+      sqlParams: string[] = Object.keys(data).map((key) => {
+        params[paramCounter++] = data[key];
+        return `$${paramCounter}`;
+      });
+
+    const query = `
+      INSERT INTO ${process.env.SHORT_PATHS} (${[...sqlFields]})
+      VALUES (${[...sqlParams]})
+      RETURNING ${process.env.SHORT_PATHS}.short_path;
+    `;
+
+    const insertResult = await db.task(async t => {
+      return await t.one(query, params);
+    });
+    console.log(query);
+    return {
+      body: JSON.stringify({ success: true, short_path: insertResult }),
+      headers: headers,
+      statusCode: 200
+    };
+    } catch (e) {
+    if ((e.message === 'Nothing to update') || (e.isJoi)) {
+      return badRequestResponse(e.message);
+    } else {
+      console.log('/admin/collections/update ERROR - ', e);
+      return internalServerErrorResponse();
+    }
   }
 };
