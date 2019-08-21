21271a9dadc9f649c89874ad419af8c907a46972
Zara <zara@acrossthecloud.net>
2019-08-22 08:35:51 +1000

    Changed let to const

--- src/pages/homepage.ts
+++ src/pages/homepage.ts
@@ -30,18 +30,14 @@ export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult
       id: Joi.array().items(Joi.number().integer()),
       oa_highlight: Joi.boolean().required()
     }));
-
+    // calculated timestamp - 1 year ago
     const
       queryString = event.queryStringParameters,
       params = [limitQuery(queryString.itemsLimit, 50), limitQuery(queryString.oaHighlightLimit, 5), limitQuery(queryString.collectionsLimit, 50), queryString.date];
-    let
-      itemsQuery,
-      collectionsQuery,
-      oaHighlightQuery;
 
     if (queryString.oa_highlight && queryString.oa_highlight.includes('true')) {
 
-      oaHighlightQuery = `
+     const oaHighlightQuery = `
         SELECT id, title, s3_key, item_subtype as type, created_at as date
         FROM tba21.${process.env.ITEMS_TABLE}
         WHERE created_at >= $4::date
@@ -51,7 +47,7 @@ export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult
         ORDER BY random()
         LIMIT $2
     `;
-      return successResponse({
+     return successResponse({
          oa_highlight: await db.any(oaHighlightQuery, params)
        });
     } else {
@@ -60,7 +56,7 @@ export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult
         whereStatement = eventParams.id.map( id => `AND id <> ${id}`).toString().replace(/,/g, ' ');
       }
       params.push(whereStatement);
-      itemsQuery = `
+      const itemsQuery = `
         SELECT id, title, s3_key, item_subtype as type, created_at as date
         FROM ${process.env.ITEMS_TABLE}
         WHERE created_at >= $4::date
@@ -72,21 +68,21 @@ export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult
         LIMIT $1
       `;
 
-      collectionsQuery = `
-      SELECT COUNT(*), 
-        ${process.env.COLLECTIONS_ITEMS_TABLE}.collection_id as id, 
-        ${process.env.COLLECTIONS_TABLE}.type, 
-        ${process.env.COLLECTIONS_TABLE}.created_at as date, 
-        COALESCE(json_agg(DISTINCT ${process.env.ITEMS_TABLE}.s3_key) FILTER (WHERE s3_key IS NOT NULL), '[]') AS s3_key
-      FROM tba21.${process.env.COLLECTIONS_TABLE}
-        INNER JOIN tba21.${process.env.COLLECTIONS_ITEMS_TABLE} ON ${process.env.COLLECTIONS_TABLE}.id = ${process.env.COLLECTIONS_ITEMS_TABLE}.collection_id
-        INNER JOIN tba21.${process.env.ITEMS_TABLE} ON ${process.env.COLLECTIONS_ITEMS_TABLE}.item_s3_key = ${process.env.ITEMS_TABLE}.s3_key
-      WHERE ${process.env.COLLECTIONS_TABLE}.created_at >= $4::date
-        AND ${process.env.COLLECTIONS_TABLE}.created_at <=  now()
-        AND ${process.env.COLLECTIONS_TABLE}.status = true
-      GROUP BY ${process.env.COLLECTIONS_TABLE}.id, ${process.env.COLLECTIONS_ITEMS_TABLE}.collection_id
-      ORDER BY random()
-      LIMIT $3
+      const collectionsQuery = `
+        SELECT COUNT(*), 
+          ${process.env.COLLECTIONS_ITEMS_TABLE}.collection_id as id, 
+          ${process.env.COLLECTIONS_TABLE}.type, 
+          ${process.env.COLLECTIONS_TABLE}.created_at as date, 
+          COALESCE(json_agg(DISTINCT ${process.env.ITEMS_TABLE}.s3_key) FILTER (WHERE s3_key IS NOT NULL), '[]') AS s3_key
+        FROM tba21.${process.env.COLLECTIONS_TABLE}
+          INNER JOIN tba21.${process.env.COLLECTIONS_ITEMS_TABLE} ON ${process.env.COLLECTIONS_TABLE}.id = ${process.env.COLLECTIONS_ITEMS_TABLE}.collection_id
+          INNER JOIN tba21.${process.env.ITEMS_TABLE} ON ${process.env.COLLECTIONS_ITEMS_TABLE}.item_s3_key = ${process.env.ITEMS_TABLE}.s3_key
+        WHERE ${process.env.COLLECTIONS_TABLE}.created_at >= $4::date
+          AND ${process.env.COLLECTIONS_TABLE}.created_at <=  now()
+          AND ${process.env.COLLECTIONS_TABLE}.status = true
+        GROUP BY ${process.env.COLLECTIONS_TABLE}.id, ${process.env.COLLECTIONS_ITEMS_TABLE}.collection_id
+        ORDER BY random()
+        LIMIT $3
       `;
       return successResponse(
         {
