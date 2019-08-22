617bd5873fd29a08a38140da5ffe58bdc884a775
Zara <zara@acrossthecloud.net>
2019-08-22 10:52:12 +1000

    OC-210 date is no longer required

--- src/pages/homepage.ts
+++ src/pages/homepage.ts
@@ -6,8 +6,13 @@ import Joi from '@hapi/joi';
 
 /**
  *
- *  Returns items and collections between a given date and now or an oa highlight between a given date
- *  and now if oa_highlight is true. Everything is returned in random order.
+ *  If a date is given it returns items and collections between the given date and now.
+ *
+ *  If oa_highlight is true, only items with oa_highlight = true are returned.
+ *
+ *  If not date is given, only items, collections and oa_highlights from within the past year will be returned.
+ *
+ *  Everything is returned in random order.
  *
  * @param event {APIGatewayEvent}
  *
@@ -26,22 +31,35 @@ export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult
       itemsLimit: Joi.number().integer(),
       collectionsLimit: Joi.number().integer(),
       oaHighlightLimit: Joi.number().integer(),
-      date: Joi.date().raw().required(),
+      date: Joi.date().raw(),
       id: Joi.array().items(Joi.number().integer()),
       oa_highlight: Joi.boolean().required()
     }));
-    // calculated timestamp - 1 year ago
+
     const
       queryString = event.queryStringParameters,
-      params = [limitQuery(queryString.itemsLimit, 50), limitQuery(queryString.oaHighlightLimit, 5), limitQuery(queryString.collectionsLimit, 50), queryString.date];
+      params = [limitQuery(queryString.itemsLimit, 50), limitQuery(queryString.oaHighlightLimit, 5), limitQuery(queryString.collectionsLimit, 50)];
 
-    if (queryString.oa_highlight && queryString.oa_highlight.includes('true')) {
+    let date: string;
+    let collectionsDate: string;
+    if ( queryString.date && queryString.date.length ) {
+      date = `
+        WHERE created_at >= '${queryString.date}'::date
+        AND created_at <= now() 
+      `;
+      params.push(date);
+    } else {
+      date = `
+        WHERE created_at >= (now() - INTERVAL '1 year')
+      `;
+      params.push(date);
+    }
+    if (queryString.oa_highlight && queryString.oa_highlight === 'true') {
 
      const oaHighlightQuery = `
         SELECT id, title, s3_key, item_subtype as type, created_at as date
         FROM tba21.${process.env.ITEMS_TABLE}
-        WHERE created_at >= $4::date
-          AND created_at <= now()
+          $4:raw
           AND status = true
           AND oa_highlight = true
         ORDER BY random()
@@ -51,7 +69,7 @@ export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult
          oa_highlight: await db.any(oaHighlightQuery, params)
        });
     } else {
-      let whereStatement = '';
+      let whereStatement: string = ``;
       if (eventParams.hasOwnProperty('id') && eventParams.id.length) {
         whereStatement = eventParams.id.map( id => `AND id <> ${id}`).toString().replace(/,/g, ' ');
       }
@@ -59,15 +77,25 @@ export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult
       const itemsQuery = `
         SELECT id, title, s3_key, item_subtype as type, created_at as date
         FROM ${process.env.ITEMS_TABLE}
-        WHERE created_at >= $4::date
-          AND created_at <= now()
+          $4:raw
           AND status = true
           AND oa_highlight IS NOT TRUE
           $5:raw
         ORDER BY random()
         LIMIT $1
       `;
-
+      if ( queryString.date && queryString.date.length ) {
+          collectionsDate = `
+          WHERE ${process.env.COLLECTIONS_TABLE}.created_at >= '${queryString.date}'::date
+          AND ${process.env.COLLECTIONS_TABLE}.created_at <= now() 
+        `;
+          params.push(collectionsDate);
+      } else {
+          collectionsDate = `
+          WHERE ${process.env.COLLECTIONS_TABLE}.created_at >= (now() - INTERVAL '1 year')
+        `;
+          params.push(collectionsDate);
+      }
       const collectionsQuery = `
         SELECT COUNT(*), 
           ${process.env.COLLECTIONS_ITEMS_TABLE}.collection_id as id, 
@@ -77,8 +105,7 @@ export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult
         FROM tba21.${process.env.COLLECTIONS_TABLE}
           INNER JOIN tba21.${process.env.COLLECTIONS_ITEMS_TABLE} ON ${process.env.COLLECTIONS_TABLE}.id = ${process.env.COLLECTIONS_ITEMS_TABLE}.collection_id
           INNER JOIN tba21.${process.env.ITEMS_TABLE} ON ${process.env.COLLECTIONS_ITEMS_TABLE}.item_s3_key = ${process.env.ITEMS_TABLE}.s3_key
-        WHERE ${process.env.COLLECTIONS_TABLE}.created_at >= $4::date
-          AND ${process.env.COLLECTIONS_TABLE}.created_at <=  now()
+          $6:raw
           AND ${process.env.COLLECTIONS_TABLE}.status = true
         GROUP BY ${process.env.COLLECTIONS_TABLE}.id, ${process.env.COLLECTIONS_ITEMS_TABLE}.collection_id
         ORDER BY random()
@@ -95,5 +122,4 @@ export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult
     console.log('/items/items.homepage ERROR - ', !e.isJoi ? e : e.details);
     return badRequestResponse();
   }
-
 };
