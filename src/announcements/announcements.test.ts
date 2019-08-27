92e858060cff184698616c421754f70fc718b480
Zara <zara@acrossthecloud.net>
2019-08-28 09:59:42 +1000

    OC-221 tests fixed

--- src/announcements/announcements.test.ts
+++ src/announcements/announcements.test.ts
@@ -7,11 +7,21 @@ require('dotenv').config(
 import { QueryStringParameters } from '../types/_test_';
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { get } from './announcements';
+import { db } from '../databaseConnect';
+import { reSeedDatabase } from '../utils/testHelper';
 
-test('Get an announcement by its id', async () => {
-  const
-    queryStringParameters: QueryStringParameters = {id: '1'},
-    response = await get({queryStringParameters } as APIGatewayProxyEvent),
-    results = JSON.parse(response.body);
-  expect(results.announcement.id).toEqual('1');
+// AfterAll tests reseed the DB
+afterAll( async () => {
+  await reSeedDatabase();
+  // Close the database connection.
+  db.$pool.end();
+});
+describe('src/announcements', () => {
+  test('Get an announcement by its id', async () => {
+    const
+      queryStringParameters: QueryStringParameters = {id: '1'},
+      response = await get({queryStringParameters } as APIGatewayProxyEvent),
+      results = JSON.parse(response.body);
+    expect(results.announcement.id).toEqual('1');
+  });
 });
