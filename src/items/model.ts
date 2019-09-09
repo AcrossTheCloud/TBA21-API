456973d455ff229da42bdfc6a1c51f45253afe58
Mehrdad Amirghasemi <mehrdad.amirghasemi@gmail.com>
2019-09-09 16:57:38 +1000

    WIP Items refactoring

--- src/items/model.ts
+++ src/items/model.ts
@@ -2,6 +2,103 @@ import { badRequestResponse, headers, internalServerErrorResponse, successRespon
 import { db } from '../databaseConnect';
 import { changeS3ProtectionLevel } from '../utils/AWSHelper';
 
+export const getAll = async (limit, offset, isAdmin: Boolean, inputQuery?) => {
+    try {
+
+        const
+            params = [limit, offset];
+
+        let searchQuery = '';
+
+        if (isAdmin && inputQuery && inputQuery.length > 0) {
+            params.push(inputQuery);
+
+            searchQuery = `
+          WHERE 
+            LOWER(title) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(original_title) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(event_title) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(subtitle) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(description) LIKE '%' || LOWER($3) || '%' OR
+
+            LOWER(institution) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(news_outlet) LIKE '%' || LOWER($3) || '%' OR
+        
+            LOWER(array_to_string(regions, '||')) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(location) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(city_of_publication) LIKE '%' || LOWER($3) || '%' OR
+        
+            LOWER(featured_in) LIKE '%' || LOWER($3) || '%' OR
+        
+            LOWER(editor) LIKE '%' || LOWER($3) || '%' OR
+        
+            ISBN::text LIKE '%' || ($3) || '%' OR
+            related_ISBN::text LIKE '%' || ($3) || '%' OR
+            LOWER(DOI) LIKE '%' || LOWER($3) || '%' OR
+        
+            LOWER(array_to_string(cast_, '||')) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(lecturer) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(project) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(record_label) LIKE '%' || LOWER($3) || '%' OR
+        
+            LOWER(array_to_string(creators, '||')) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(array_to_string(directors, '||')) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(array_to_string(writers, '||')) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(array_to_string(collaborators, '||')) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(array_to_string(authors, '||')) LIKE '%' || LOWER($3) || '%' OR
+        
+            LOWER(array_to_string(publisher, '||')) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(array_to_string(produced_by, '||')) LIKE '%' || LOWER($3) || '%' OR
+        
+            LOWER(array_to_string(participants, '||')) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(array_to_string(interviewers, '||')) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(array_to_string(interviewees, '||')) LIKE '%' || LOWER($3) || '%' OR
+        
+            LOWER(array_to_string(speakers, '||')) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(array_to_string(performers, '||')) LIKE '%' || LOWER($3) || '%' OR
+        
+            LOWER(array_to_string(host_organisation, '||')) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(array_to_string(organisation, '||')) LIKE '%' || LOWER($3) || '%' OR
+            
+            LOWER(concept_tag.tag_name) LIKE '%' || LOWER($3) || '%' OR
+            LOWER(keyword_tag.tag_name) LIKE '%' || LOWER($3) || '%' 
+  
+        `;
+        }
+
+        const
+            query = `
+          SELECT
+            COUNT ( item.s3_key ) OVER (),
+            item.*,
+            COALESCE(json_agg(DISTINCT concept_tag.*) FILTER (WHERE concept_tag IS NOT NULL), '[]') AS aggregated_concept_tags,
+            COALESCE(json_agg(DISTINCT keyword_tag.*) FILTER (WHERE keyword_tag IS NOT NULL), '[]') AS aggregated_keyword_tags,
+            ST_AsGeoJSON(item.geom) as geoJSON
+          FROM 
+            ${process.env.ITEMS_TABLE} AS item,
+              
+            UNNEST(CASE WHEN item.concept_tags <> '{}' THEN item.concept_tags ELSE '{null}' END) AS concept_tagid
+              LEFT JOIN ${process.env.CONCEPT_TAGS_TABLE} AS concept_tag ON concept_tag.ID = concept_tagid,
+                      
+            UNNEST(CASE WHEN item.keyword_tags <> '{}' THEN item.keyword_tags ELSE '{null}' END) AS keyword_tagid
+              LEFT JOIN ${process.env.KEYWORD_TAGS_TABLE} AS keyword_tag ON keyword_tag.ID = keyword_tagid
+              
+          ${searchQuery}         
+              
+          GROUP BY item.s3_key
+          ORDER BY item.updated_at DESC NULLS LAST
+  
+          LIMIT $1 
+          OFFSET $2 
+        `;
+
+        return successResponse({ items: await db.any(query, params) });
+    } catch (e) {
+        console.log('items/model.get ERROR - ', e);
+        return badRequestResponse();
+    }
+};
+
 export const update = async (requestBody, isAdmin: Boolean, userId?: String) => {
     try {
 
@@ -107,13 +204,13 @@ export const deleteItm = async (s3Key, isAdmin: Boolean, userId?: String) => {
         }
 
         if (delResult) {
-            await db.any(`
-                          DELETE FROM ${process.env.SHORT_PATHS_TABLE}
+            await db.any(
+                `DELETE FROM ${process.env.SHORT_PATHS_TABLE}
                           WHERE EXISTS (
                             SELECT * FROM ${process.env.SHORT_PATHS_TABLE}
                             WHERE object_type = 'Item'
                             AND id = $1 )`,
-                         [delResult.id]);
+                [delResult.id]);
         }
 
         return successResponse(true);
