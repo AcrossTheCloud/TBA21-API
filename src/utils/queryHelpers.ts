6524a46094e04d14244da3c8596710895491ccc5
Matthew Berryman <matthew@acrossthecloud.net>
2019-09-21 14:49:40 +1000

    OC-241 fix limit logic

--- src/utils/queryHelpers.ts
+++ src/utils/queryHelpers.ts
@@ -1,3 +1,3 @@
 export const limitQuery = (limit, defaultLimit): string => {
-  return `${(parseInt(limit, 0) < 50 ? parseInt(limit, 0) : parseInt(defaultLimit, 0))}`;
+  return `${((limit && parseInt(limit, 0)) ? parseInt(limit, 0) : parseInt(defaultLimit, 0))}`;
 };
