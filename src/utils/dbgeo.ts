4a62440817fa2df00a06874589505b5058361346
Matthew Berryman <matthew@acrossthecloud.net>
2019-10-11 06:25:14 +1100

    linter fixes + typings

--- src/utils/dbgeo.ts
+++ src/utils/dbgeo.ts
@@ -1,4 +1,5 @@
 import * as dbgeo from 'dbgeo';
+import { TopoJSON } from 'topojson-specification';
 
 /* set up dbgeo defaults
  * outputFormat: all the goodness of topojson
@@ -12,14 +13,24 @@ dbgeo.defaults = {
   geometryType: 'wkt',
   precision: null,
   quantization: null
+};
+
+interface DbGeoParams {
+  outputFormat?: string;
+  geometryColumn?: string;
+  geometryType?: string;
+  precision?: number | null;
+  quantization?: number | null;
 }
 
-export const dbgeoparse = function(input, params) {
+export const dbgeoparse = function(input: any, params: DbGeoParams) { // tslint:disable-line:no-any
   return new Promise((resolve, reject) => {
-    dbgeo.parse(input, params, (err, output) => {
-      if (err) reject(err)
-      else resolve(output);
+    dbgeo.parse(input, params, (err: string, output: TopoJSON) => { 
+      if (err) {
+        reject(err);
+      } else {
+        resolve(output);
+      }
     });
-  })
-}
-
+  });
+};
