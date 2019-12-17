bc1ed5a3b7a8f2e13a7da556652c6f8c13ffa079
Dan Wood <daniel@acrossthecloud.net>
2019-12-17 13:32:08 +1100

    OC-375 Fixed tests as they were using the old collection in collections schema.

--- src/admin/collections/insert.test.ts
+++ src/admin/collections/insert.test.ts
@@ -67,9 +67,8 @@ describe('/admin/collections/insert/', () => {
     expect(nItems.count).toBe('2');
   });
 
-  test('Create a complete collection with items', async () => {
+  test('Create a complete collection with items and collections', async () => {
     const
-
       requestBody = {
         'start_date': '2019-02-22 10:53',
         'regional_focus': 'Arctic Ocean',
@@ -125,7 +124,7 @@ describe('/admin/collections/insert/', () => {
         'volume': '1',
         'number': '2',
         'items': ['private/eu-central-1:80f1e349-677b-4aed-8b26-896570a8073c/ad742900-a6a0-11e9-b5d9-1726307e8330-kitten-pet-animal-domestic-104827.jpeg', 'private/eu-central-1:80f1e349-677b-4aed-8b26-896570a8073c/ad742900-a6a0-11e9-b5d9-1726307e8330-dog-pet-animal-domestic-104827.jpeg', 'private/eu-central-1:80f1e349-677b-4aed-8b26-896570a8073c/862f0b10-a6a7-11e9-9669-7fbab4073699-Humpback_Whales_-_South_Bank.jpg'],
-        'collections' : { id : [1, 2] }
+        'collections' : [1, 2]
       },
       body: string = JSON.stringify(requestBody),
       response = await createCollection({ body, 'path': '/admin/collections/create' } as APIGatewayProxyEvent),
