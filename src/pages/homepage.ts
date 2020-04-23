3e954f3c24771b0c4c8480a086a90ee1c09071f0
Matthew Berryman <matthew@acrossthecloud.net>
2020-04-23 11:23:44 +1000

    homepage API changes for collections oa_highlights

--- src/pages/homepage.ts
+++ src/pages/homepage.ts
@@ -20,11 +20,9 @@ import Joi from '@hapi/joi';
  */
 export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   try {
-
-    const ids = event.multiValueQueryStringParameters && event.multiValueQueryStringParameters.hasOwnProperty('id') ? {id: event.multiValueQueryStringParameters.id} : {};
+    
     const eventParams = {
-      ...event.queryStringParameters,
-      ...ids
+      ...event.queryStringParameters
     };
 
     await Joi.assert(eventParams, Joi.object().keys({
@@ -32,7 +30,6 @@ export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult
       collectionsLimit: Joi.number().integer(),
       oaHighlightLimit: Joi.number().integer(),
       date: Joi.date().raw(),
-      id: Joi.array().items(Joi.number().integer()),
       oa_highlight: Joi.boolean().required()
     }));
 
@@ -59,7 +56,7 @@ export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult
 
     if (queryString.oa_highlight && queryString.oa_highlight === 'true') {
 
-     const oaHighlightQuery = `
+     const oaItemsHighlightQuery = `
         SELECT 
         items.id,
         title,
@@ -89,15 +86,39 @@ export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult
         LIMIT $2:raw
     `;
 
+        // Collections
+        const collectionsQuery = `
+        SELECT COUNT(*), 
+          ${process.env.COLLECTIONS_ITEMS_TABLE}.collection_id as id,
+          ${process.env.COLLECTIONS_TABLE}.title,
+          ${process.env.COLLECTIONS_TABLE}.type, 
+          ${process.env.COLLECTIONS_TABLE}.time_produced,
+          ${process.env.COLLECTIONS_TABLE}.year_produced, 
+          COALESCE(array_agg(DISTINCT ${process.env.COLLECTIONS_ITEMS_TABLE}.item_s3_key)) AS s3_key,
+          ${process.env.COLLECTIONS_TABLE}.creators,
+          ${process.env.COLLECTIONS_TABLE}.regions
+        FROM ${process.env.COLLECTIONS_TABLE}
+          INNER JOIN ${process.env.COLLECTIONS_ITEMS_TABLE} ON ${process.env.COLLECTIONS_TABLE}.id = ${process.env.COLLECTIONS_ITEMS_TABLE}.collection_id
+          INNER JOIN ${process.env.ITEMS_TABLE} ON ${process.env.COLLECTIONS_ITEMS_TABLE}.item_s3_key = ${process.env.ITEMS_TABLE}.s3_key
+          WHERE ${process.env.COLLECTIONS_TABLE}.status = true
+          AND ${process.env.ITEMS_TABLE}.status = true
+          AND ${process.env.COLLECTIONS_TABLE}.oa_highlight = true
+          $6:raw
+        GROUP BY ${process.env.COLLECTIONS_TABLE}.id, ${process.env.COLLECTIONS_ITEMS_TABLE}.collection_id
+        ORDER BY random()
+        LIMIT $3:raw
+      `;
+
+      let collectionsResult = await db.any(collectionsQuery, params);
+
+      // Remove all collections without an item.
+      collectionsResult = collectionsResult.filter(c => c.s3_key && c.s3_key.length);
+
      return successResponse({
-         oa_highlight: await db.any(oaHighlightQuery, params)
+         oa_highlight_items: await db.any(oaItemsHighlightQuery, params),
+         oa_highlight_collections: collectionsResult
        });
     } else {
-      let whereStatement: string = ``;
-      if (eventParams.hasOwnProperty('id') && eventParams.id.length) {
-        whereStatement = eventParams.id.map( id => `AND items.id <> ${id}`).toString().replace(/,/g, ' ');
-      }
-      params.push(whereStatement);
       const itemsQuery = `
         SELECT 
           items.id,
@@ -151,6 +172,7 @@ export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult
           INNER JOIN ${process.env.COLLECTIONS_ITEMS_TABLE} ON ${process.env.COLLECTIONS_TABLE}.id = ${process.env.COLLECTIONS_ITEMS_TABLE}.collection_id
           INNER JOIN ${process.env.ITEMS_TABLE} ON ${process.env.COLLECTIONS_ITEMS_TABLE}.item_s3_key = ${process.env.ITEMS_TABLE}.s3_key
           WHERE ${process.env.COLLECTIONS_TABLE}.status = true
+          AND ${process.env.COLLECTIONS_TABLE}.oa_highlight = false
           AND ${process.env.ITEMS_TABLE}.status = true
           $6:raw
         GROUP BY ${process.env.COLLECTIONS_TABLE}.id, ${process.env.COLLECTIONS_ITEMS_TABLE}.collection_id
