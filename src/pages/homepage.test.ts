a2369eaa5c3d32bba31c42c6eebd2bebc2ce47ea
Matthew Berryman <matthew@acrossthecloud.net>
2020-04-27 10:16:23 +1000

    updated and added tests and seeds

--- src/pages/homepage.test.ts
+++ src/pages/homepage.test.ts
@@ -3,7 +3,7 @@ require('dotenv').config(
     DEBUG: true
   });
 
-import { MultiQueryStringParameters, QueryStringParameters } from '../types/_test_';
+import { QueryStringParameters } from '../types/_test_';
 import { get } from './homepage';
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { db } from '../databaseConnect';
@@ -15,13 +15,21 @@ describe('homepage tests', () => {
     db.$pool.end();
   });
 
-  test('Get items and collections between dates', async () => {
+  test('Get non oa_highlight items and collections between dates', async () => {
     const
       queryStringParameters: QueryStringParameters = {date: '2011-07-01',  oa_highlight: 'false'},
       response = await get({ queryStringParameters } as APIGatewayProxyEvent),
       results = JSON.parse(response.body);
-    expect(results.items.length).toEqual(4);
-    expect(results.collections.length).toEqual(3);
+    expect(results.items.length).toEqual(3);
+    expect(results.collections.length).toEqual(2);
+  });
+  test('Get oa_highlights', async () => {
+    const
+    queryStringParameters: QueryStringParameters = {oa_highlight: 'true'},
+    response = await get({ queryStringParameters } as APIGatewayProxyEvent),
+    results = JSON.parse(response.body);
+  expect(results.oa_highlight_items.length).toEqual(1);
+  expect(results.oa_highlight_collections.length).toEqual(1);
   });
   test('Test we can limit what we get back', async () => {
     const
@@ -29,6 +37,6 @@ describe('homepage tests', () => {
       response = await get({ queryStringParameters } as APIGatewayProxyEvent),
       results = JSON.parse(response.body);
     expect(results.items.length).toEqual(1);
-    expect(results.collections.length).toEqual(3);
+    expect(results.collections.length).toEqual(2);
   });
 });
