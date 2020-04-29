1c7ef48cb5bd97a88c883ed364341e38f2f553d4
Matthew Berryman <matthew@acrossthecloud.net>
2020-04-29 14:24:55 +1000

    remove funky filter

--- src/pages/homepage.ts
+++ src/pages/homepage.ts
@@ -141,7 +141,6 @@ export const get = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult
           WHERE status = true
           AND oa_highlight IS NOT TRUE
           AND s3_key not in (select item_s3_key from tba21.collections_items)
-          AND NOT (s3_key ilike '%.mp3' or item_type = 'Audio')
           AND on_homepage = true
           $3:raw
         GROUP BY items.id, items.title, items.s3_key
